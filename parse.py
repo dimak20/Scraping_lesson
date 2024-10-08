a75625ea671bbbab1fbc7443e6d257b631aa6ba8
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-10-08 15:24:45 +0300

    Initial commit

---
+++ parse.py
@@ -0,0 +1,100 @@
+import csv
+import logging
+import sys
+from dataclasses import dataclass, fields, astuple
+from typing import List
+from urllib.parse import urljoin
+
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://webscraper.io/"
+HOME_URL = urljoin(BASE_URL, "test-sites/e-commerce/allinone")
+
+PRODUCTS_OUTPUT_CSV_PATH = "products.csv"
+
+
+@dataclass
+class Product:
+    title: str
+    description: str
+    price: float
+    rating: int
+    num_of_reviews: int
+
+
+PRODUCT_FIELDS = [field.name for field in fields(Product)]
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="[%(levelname)8s]: %(message)s",
+    handlers=[
+        logging.FileHandler("parser.log"),
+        logging.StreamHandler(sys.stdout),
+    ],
+)
+
+
+# [attr for attr in dir(obj)]
+
+def parse_single_product(product_soup: BeautifulSoup) -> Product:
+    return (Product(
+        title=product_soup.select_one("a.title")["title"],
+        description=product_soup.select_one(".description").text,
+        price=float(product_soup.select_one(".price").text.replace("$", "")),
+        rating=int(product_soup.select_one("p[data-rating]")["data-rating"]),
+        num_of_reviews=int(product_soup.select_one(
+            "div.ratings > .review-count"
+        ).text.split()[0])
+    ))
+
+
+def get_num_pages(page_soup: BeautifulSoup) -> int:
+    pagination = page_soup.select_one(".pagination")
+
+    if pagination is None:
+        return 1
+
+    return int(pagination.select("li")[-2].text)
+
+
+def get_single_page_products(page_soup: BeautifulSoup) -> list[Product]:
+    products = page_soup.select(".thumbnail")
+
+    return [parse_single_product(product_soup) for product_soup in products]
+
+
+def get_home_products() -> List[Product]:
+    logging.info(f"Start parsing laptops")
+    page = requests.get(HOME_URL).content
+    first_page_soup = BeautifulSoup(page, "html.parser")
+
+    # get nums of pages
+    num_pages = get_num_pages(first_page_soup)
+
+    all_products = get_single_page_products(first_page_soup)
+
+    # iterate on pages & get all products from single page
+    for page_num in range(2, num_pages + 1):
+        logging.info(f"Start parsing page #{page_num}")
+        page = requests.get(HOME_URL, {"page": page_num}).content
+        soup = BeautifulSoup(page, "html.parser")
+        all_products.extend(get_single_page_products(soup))
+
+    return all_products
+
+
+def write_products_to_csv(products: list[Product]) -> None:
+    with open(PRODUCTS_OUTPUT_CSV_PATH, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(PRODUCT_FIELDS)
+        writer.writerows([astuple(product) for product in products])
+
+
+def main():
+    products = get_home_products()
+    write_products_to_csv(products)
+
+
+if __name__ == '__main__':
+    main()
